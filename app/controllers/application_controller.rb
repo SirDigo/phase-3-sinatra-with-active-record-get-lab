6c547961fb2c0b5b4a391e5081c3d4893bbab4eb
Caleb Rodriguez <sirdigo6@gmail.com>
2022-01-24 13:19:36 -0500

    commit tree dab14fa72a98d82c2ed4c9cdafbfd65b32ca3655

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, "application/json"
+
+  get "/bakeries" do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get "/bakeries/:id" do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods/by_price" do
+    ordered = BakedGood.order(price: :desc)
+    ordered.to_json
+  end
+
+  get "/baked_goods/most_expensive" do
+    pricy = BakedGood.order(price: :desc).first
+    pricy.to_json
+  end
 
 end
